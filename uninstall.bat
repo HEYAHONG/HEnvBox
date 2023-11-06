f54530f45ebd7d98a828df5cff1ad9a660e95077
HEYAHONG <2229388563@qq.com>
2023-11-06 12:33:10 +0800

    添加MSYS2的入口

--- uninstall.bat
+++ uninstall.bat
@@ -9,6 +9,13 @@ call "%HENVBOX_ROOT_PATH%\config.bat"
 
 @rem 删除右键菜单
 call :RemoveMenu HEnvBox
+call :RemoveMenu HEnvBoxMSYS2
+call :RemoveMenu HEnvBoxMINGW32
+call :RemoveMenu HEnvBoxMINGW64
+call :RemoveMenu HEnvBoxCLANG32
+call :RemoveMenu HEnvBoxCLANG64
+call :RemoveMenu HEnvBoxCLANGARM64
+call :RemoveMenu HEnvBoxUCRT64
 
 @rem 删除右键菜单
 @rem 参数1：在注册表中的名称(不可带引号)，参数2:显示名称，参数3：命令

7c3f6a5fb0e0b05bc04dfb1c4c64f08523fbeec9
HEYAHONG <2229388563@qq.com>
2023-11-06 11:58:13 +0800

    完善右键菜单安装卸载

--- install.bat
+++ install.bat
@@ -13,7 +13,17 @@ if exist "%HENVBOX_ROOT_PATH%\tools\%HENVBOX_TYPE%\install.bat" call "%HENVBOX_R
 if x%Failure%==x1 pause
 
 @rem 添加右键菜单
-reg add HKCR\Directory\shell\HEnvBox /t REG_SZ /f  /ve /d "HEnvBox"
-reg add HKCR\Directory\shell\HEnvBox\command /t REG_SZ /f  /ve /d "cmd.exe /k call %HENVBOX_ROOT_PATH%\config.bat"
-reg add HKCR\Directory\Background\shell\HEnvBox /t REG_SZ /f  /ve /d "HEnvBox"
-reg add HKCR\Directory\Background\shell\HEnvBox\command /t REG_SZ /f  /ve /d "cmd.exe /k call %HENVBOX_ROOT_PATH%\config.bat"
+call :AddMenu HEnvBox "HEnvBox Cmd" "cmd.exe /k call %HENVBOX_ROOT_PATH%\config.bat"
+
+@rem 添加右键菜单
+@rem 参数1：在注册表中的名称(不可带引号)，参数2:显示名称，参数3：命令
+goto :eof
+:AddMenu
+set REG_NAME=%1
+set REG_DISPLAY_NAME=%2
+set REG_CMD=%3
+reg add "HKCR\Directory\shell\%REG_NAME%" /t REG_SZ /f  /ve /d %REG_DISPLAY_NAME%
+reg add "HKCR\Directory\shell\%REG_NAME%\command" /t REG_SZ /f  /ve /d %REG_CMD%
+reg add "HKCR\Directory\Background\shell\%REG_NAME%"  /t REG_SZ /f  /ve /d %REG_DISPLAY_NAME%
+reg add "HKCR\Directory\Background\shell\%REG_NAME%\command" /t REG_SZ /f  /ve /d %REG_CMD%
+goto :eof
--- uninstall.bat
+++ uninstall.bat
@@ -7,6 +7,14 @@ set HENVBOX_ROOT_PATH=%~dp0
 call "%HENVBOX_ROOT_PATH%\config.bat"
 
 
-@rem 添加右键菜单
-reg delete HKCR\Directory\shell\HEnvBox  /f  
-reg delete HKCR\Directory\Background\shell\HEnvBox  /f  
+@rem 删除右键菜单
+call :RemoveMenu HEnvBox
+
+@rem 删除右键菜单
+@rem 参数1：在注册表中的名称(不可带引号)，参数2:显示名称，参数3：命令
+goto :eof
+:RemoveMenu
+set REG_NAME=%1
+reg delete "HKCR\Directory\shell\%REG_NAME%"  /f  
+reg delete "HKCR\Directory\Background\shell\%REG_NAME%"  /f  
+goto :eof

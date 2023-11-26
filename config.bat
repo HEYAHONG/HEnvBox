5841795035757dbc3a4606ee40e12b3711eac782
HEYAHONG <2229388563@qq.com>
2023-11-26 17:31:20 +0800

    自动启用某些目录的大小写支持

--- config.bat
+++ config.bat
@@ -21,7 +21,8 @@ set HENVBOX_LOCAL_BINDIR_PATH=%HENVBOX_LOCAL_BINDIR_PATH:"=%
 set HENVBOX_LOCAL_ROOT_PATH_UNIX=%HENVBOX_LOCAL_ROOT_PATH:\=/%
 set HENVBOX_LOCAL_BINDIR_PATH_UNIX=%HENVBOX_LOCAL_BINDIR_PATH:\=/%
 call :AddToPATH %HENVBOX_LOCAL_BINDIR_PATH%
-
+call :setCaseSensitiveInfo "%HENVBOX_LOCAL_ROOT_PATH%" enable
+call :setCaseSensitiveInfo "%HENVBOX_LOCAL_BINDIR_PATH%" disable
 
 @rem 添加到PATH环境变量
 goto :eof
@@ -30,4 +31,29 @@ set NEW_PATH_ITEM=%1
 set NEW_PATH_ITEM=%NEW_PATH_ITEM:/=\%
 set NEW_PATH_ITEM_STRIP=%NEW_PATH_ITEM:\\=\%
 set PATH=%NEW_PATH_ITEM_STRIP%;%PATH%
+goto :eof
+
+
+@rem 启用目录大小写(参数1为目录路径)
+goto :eof
+:setCaseSensitiveInfo
+set DIR_DEPTH=0
+set DIR_MAX_DEPTH=4
+call :setCaseSensitiveInfoImp "%1" %2
+goto :eof
+:setCaseSensitiveInfoImp
+if not exist "%1" goto :eof
+pushd  "%1" 2> /nul > nul
+
+set /a DIR_DEPTH=%DIR_DEPTH%+1
+if "X%DIR_DEPTH%"=="X%DIR_MAX_DEPTH%" goto :setCaseSensitiveInfoImpExit
+
+fsutil file setCaseSensitiveInfo . %2  2> nul  > nul
+
+for /f %%I in ('dir /a:d /b') do (
+call :setCaseSensitiveInfoImp %%I %2
+)
+:setCaseSensitiveInfoImpExit
+set /a DIR_DEPTH=%DIR_DEPTH%-1
+popd 2> nul  > nul 
 goto :eof

c4e413971123aaf1bf588df0ca6191f7c2d52b36
HEYAHONG <2229388563@qq.com>
2024-02-15 20:27:07 +0800

    [EnvCheck] 添加EnvCheckLog函数

--- tools/EnvCheck/EnvCheck.d/00-common.sh
+++ tools/EnvCheck/EnvCheck.d/00-common.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+#打印EnvCheck日志,格式同printf命令
+function EnvCheckLog
+{
+	if [ "${CONFIG_DISABLE_ENVCHECK_LOG}" = "y" ]
+	then
+		return 0
+	fi
+	if [ -z "$*" ]
+	then
+		return 1
+	fi
+	printf -v MSG "$@" 2> /dev/null
+	printf -v MSG '[EnvCheck] %s' "${MSG}" 2> /dev/null
+	echo  ${MSG}
+	return 0
+}
+
 #测试程序(通过which测试)，参数1为程序名
 function EnvCheckTestProgram
 {

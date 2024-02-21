fb4b9325a352e1bb3eb6f73ee9ed7298f67d4b6c
HEYAHONG <2229388563@qq.com>
2024-02-21 20:58:35 +0800

    添加常用环境变量

--- tools/common/include/Makefile/prepare-env.mk
+++ tools/common/include/Makefile/prepare-env.mk
@@ -1,6 +1,63 @@
 # prepare-env.mk
 # 由top.mk包含
 
+#定义主机工具(默认主机上采用gcc)
+HOSTCC  	?= cc
+HOSTCXX  	?= c++
+
+HOSTCFLAGS	:= $(HOSTCFLAGS)
+HOSTCXXFLAGS	:= $(HOSTCXXFLAGS)
+HOSTAFLAGS	:= $(HOSTAFLAGS)
+HOSTLDFLAGS	:= $(HOSTLDFLAGS)
+
+export HOSTCC HOSTCXX HOSTCFLAGS HOSTCXXFLAGS HOSTAFLAGS HOSTLDFLAGS
+
+#定义工具链
+ifeq (${CROSS_COMPILE},)
+
+AS              ?= as
+CC              ?= cc
+CPP             ?= c++
+LD              ?= $(CPP)
+AR              ?= ar
+NM              ?= nm
+STRIP           ?= strip
+OBJCOPY         ?= objcopy
+OBJDUMP         ?= objdump
+PKG_CONFIG      ?= pkg-config
+
+CFLAGS		:= $(CFLAGS)
+CPPFLAGS	:= $(CPPFLAGS)
+AFLAGS		:= $(AFLAGS)
+LDFLAGS		:= $(LDFLAGS)
+LDLIBS		:= $(LDLIBS)
+
+
+else
+
+AS		= $(CROSS_COMPILE)as
+CC		= $(CROSS_COMPILE)gcc
+CPP		= $(CROSS_COMPILE)g++
+LD		= $(CPP)
+AR		= $(CROSS_COMPILE)ar
+NM		= $(CROSS_COMPILE)nm
+STRIP		= $(CROSS_COMPILE)strip
+OBJCOPY		= $(CROSS_COMPILE)objcopy
+OBJDUMP		= $(CROSS_COMPILE)objdump
+PKG_CONFIG	?= $(CROSS_COMPILE)pkg-config
+
+CFLAGS          := $(CFLAGS)
+CPPFLAGS        := $(CPPFLAGS)
+AFLAGS          := $(AFLAGS)
+LDFLAGS         := $(LDFLAGS)
+LDLIBS          := $(LDLIBS)
+
+endif
+
+export AS CC CPP LD AR NM STRIP OBJCOPY OBJDUMP PKG_CONFIG
+export CFLAGS CPPFLAGS AFLAGS LDFLAGS LDLIBS
+
+
 #定义各个目标的步骤,默认为空，但用户需要将实际步骤通过+=添加至相应步骤
 prepare_step:=
 download_step:=

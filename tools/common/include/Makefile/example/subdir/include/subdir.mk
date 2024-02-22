3a83430767f57e238db9ff7e5a9bc44fae41adca
HEYAHONG <2229388563@qq.com>
2024-02-22 14:55:38 +0800

    添加子目录包含

---
+++ tools/common/include/Makefile/example/subdir/include/subdir.mk
@@ -0,0 +1,43 @@
+
+
+prepare_subdir_mk_step_1:
+	@echo prepare_subdir_mk_step_1
+
+prepare_subdir_mk_step_2:
+	@echo prepare_subdir_mk_step_2
+
+prepare_step+=prepare_subdir_mk_step_1 prepare_subdir_mk_step_2
+
+download_subdir_mk_step_1:
+	@echo download_subdir_mk_step_1
+
+download_subdir_mk_step_2:
+	@echo download_subdir_mk_step_2
+
+download_step+=download_subdir_mk_step_1 download_subdir_mk_step_2
+
+configure_subdir_mk_step_1:
+	@echo configure_subdir_mk_step_1
+
+configure_step+=configure_subdir_mk_step_1
+
+build_subdir_mk_step_1:
+	@echo build_subdir_mk_step_1
+
+build_step+=build_subdir_mk_step_1
+
+install_subdir_mk_step_1:
+	@echo install_subdir_mk_step_1
+
+install_step+=install_subdir_mk_step_1
+
+clean_subdir_mk_step_1:
+	@echo clean_subdir_mk_step_1
+
+clean_step+=clean_subdir_mk_step_1
+
+dist-clean_subdir_mk_step_1:
+	@echo dist-clean_subdir_mk_step_1
+
+dist-clean_step+=dist-clean_subdir_mk_step_1
+
--- tools/common/include/Makefile/prepare-dir.mk
+++ tools/common/include/Makefile/prepare-dir.mk
@@ -72,3 +72,45 @@ clean-dir:
 #添加至发行清理目标
 dist-clean:clean-dir
 
+
+#定义Include，使用$(eval $(call Include,需要包含的文件路径))调用
+#通过__Inclide_Included_Files__确保不重复包含
+__Inclide_Included_Files__ :=
+define Include
+
+ifneq (${1},)
+
+Include_File := $(shell realpath ${1})
+
+ifneq ($(findstring ${Include_File},${__Inclide_Included_Files__}),${Include_File})
+
+include ${Include_File}
+
+__Inclide_Included_Files__ += ${Include_File}
+
+endif
+
+endif
+
+endef
+
+#定义包含子目录,$(eval $(call IncludeSubdirMakefile,目录,可选文件名))调用，当文件名不指定时，默认为subdir.mk
+define IncludeSubdirMakefile
+
+ifneq (${2},)
+
+$(foreach file,$(shell find ${1} -type f -name ${2} 2> /dev/null),$(eval $(call Include,$(file))))
+
+else
+
+ifneq (${1},)
+
+$(foreach file,$(shell find ${1} -type f -name subdir.mk 2> /dev/null),$(eval $(call Include,$(file))))
+
+endif
+
+endif
+
+endef
+
+

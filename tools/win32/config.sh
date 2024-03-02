f16de09d5b39287eb9d1cdac3eebdcb73c6cde7c
HEYAHONG <2229388563@qq.com>
2024-03-02 12:52:42 +0800

    [win32] 修改默认crosstool-ng安装路径

--- tools/win32/config.sh
+++ tools/win32/config.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+#添加其它软件(如ct-ng编译的交叉编译工具链)
+if [ -d ${MSYSTEM_PREFIX} ]
+then
+	for bin in `find ${MSYSTEM_PREFIX}/crosstool-ng -mindepth 2 -maxdepth 2 -type d -name bin 2>/dev/null`
+	do
+		export PATH=${bin}:${PATH}
+	done
+fi
+
 #将Kconfig配置转化为环境变量
 if [ -f "${HENVBOX_TOOLS_PATH}/.config" ]
 then

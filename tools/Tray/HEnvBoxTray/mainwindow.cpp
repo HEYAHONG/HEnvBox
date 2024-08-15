8d46565f524d7c9fcaaa41513f1266d7294e9908
HEYAHONG <2229388563@qq.com>
2024-08-15 22:30:38 +0800

    添加托盘程序模板

---
+++ tools/Tray/HEnvBoxTray/mainwindow.cpp
@@ -0,0 +1,82 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+    ,app_instance_pid("app_instance_pid_henvboxtray",nullptr),
+      m_trayicon(this)
+{
+    ui->setupUi(this);
+    hide();
+    app_instance_pid_check();
+    //打开创建系统图标
+    m_trayicon.setIcon(QIcon(QString::fromUtf8(":/HEnvBox-256x256.ico")));
+    m_trayicon.show();
+    m_trayicon.setContextMenu(new QMenu(this));
+    connect(&m_trayicon,&QSystemTrayIcon::activated,this,&MainWindow::Trayiconactivated);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::app_instance_pid_check()
+{
+    int pid=QCoreApplication::applicationPid();
+    qDebug() << "current process pid is "<<pid<<"!";
+    app_instance_pid.setNativeKey("app_instance_pid_henvboxtray");
+    if(app_instance_pid.attach())
+    {
+        int pid_old=*(const int *)app_instance_pid.constData();
+        if(pid!=pid_old)
+        {
+            //TODO:检查现有的进程
+
+#ifdef __linux__
+            if(!QFile::exists(QString("/proc/")+std::to_string(pid_old).c_str()))
+            {
+                (*(int *)app_instance_pid.data())=pid;
+                return;
+            }
+#endif
+            qDebug() << "process(pid="<<pid_old<<") is running!";
+            //只允许一个实例
+            qDebug() << "Exiting" ;
+            exit(-1);
+        }
+    }
+    if(app_instance_pid.create(sizeof(int)))
+    {
+        (*(int *)app_instance_pid.data())=pid;
+    }
+}
+
+void MainWindow::Trayiconactivated(QSystemTrayIcon::ActivationReason reason)
+{
+    switch (reason)
+    {
+    case QSystemTrayIcon::Context:
+    {
+        //上下文菜单，一般指右键菜单
+    }
+    break;
+    case QSystemTrayIcon::DoubleClick:
+    {
+        //双击
+    }
+    break;
+    case QSystemTrayIcon::Trigger:
+    {
+        //点击
+    }
+    break;
+    default:
+    {
+
+    }
+    break;
+    }
+}
---
+++ tools/Tray/HEnvBoxTray/mainwindow.h
@@ -0,0 +1,34 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QSharedMemory>
+#include <QProcess>
+#include <QSystemTrayIcon>
+#include <QIcon>
+#include <QFile>
+#include <QMenu>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    QSharedMemory  app_instance_pid;
+    void app_instance_pid_check();
+    QSystemTrayIcon m_trayicon;
+public slots:
+    void 	Trayiconactivated(QSystemTrayIcon::ActivationReason reason);
+};
+#endif // MAINWINDOW_H

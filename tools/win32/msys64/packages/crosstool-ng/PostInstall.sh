20b99db587df8946fd6ade99afc9bb41ea4f3673
HEYAHONG <2229388563@qq.com>
2025-02-09 01:50:12 +0800

    [win32] 在Windows下也使用源代码编译crosstool-ng

---
+++ tools/win32/msys64/packages/crosstool-ng/PostInstall.sh
@@ -0,0 +1,56 @@
+#/bin/bash
+
+#检查是否在HEnvBox中
+[ -d "${HENVBOX_ROOT_PATH}" ] || exit 1
+
+#shellcheck disable=SC2128  # ignore array expansion warning
+if [ -n "${BASH_SOURCE-}" ]
+then
+	self_path="${BASH_SOURCE}"
+elif [ -n "${ZSH_VERSION-}" ]
+then
+	self_path="${(%):-%x}"
+else
+	exit 1
+fi
+
+# shellcheck disable=SC2169,SC2169,SC2039  # unreachable with 'dash'
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	# convert possibly relative path to absolute
+	script_dir="$(realpath_int "${self_path}")"
+	# resolve any ../ references to make the path shorter
+	script_dir="$(cd "${script_dir}" || exit 1; pwd)"
+else
+	# convert to full path and get the directory name of that
+	script_name="$(readlink -f "${self_path}")"
+	script_dir="$(dirname "${script_name}")"
+fi
+
+#测试镜像,更换国内备用镜像地址
+wget --no-check-certificate --connect-timeout=5 -O /dev/null https://github.com 2>/dev/null >/dev/null
+if [ "$?" -ne 0 ]
+then
+	echo 使用备用镜像，可能不是最新的crosstool-ng
+	export CROSSTOOL_NG_LOCAL_SRC_URL=https://gitee.com/HEYAHONG/crosstool-ng.git
+fi
+
+#进入当前目录
+pushd ${script_dir}
+
+#进行编译
+make -f crosstool-ng.mk install
+MAKE_ERRNO=$?
+
+#退出目录
+popd
+
+#提示是否成功
+if [ "${MAKE_ERRNO}" -eq "0" ]
+then
+	echo 编译成功！
+else
+	echo 编译失败！请手动重新调用安装脚本！
+fi
+
+#正常退出
+exit 0

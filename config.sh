e9fd244cc7322baa7030f09d1b146dffb277ea41
HEYAHONG <2229388563@qq.com>
2026-02-20 19:32:52 +0800

    采用shfmt格式化代码

--- config.sh
+++ config.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-function CheckTool
-{
-	[  -n "$1"  ] ||
-	{
-		echo -e  "CheckTool 参数错误!!";
-		return 255;
-	};
-	ToolPath=`which $1`;
+function CheckTool {
+	[ -n "$1" ] ||
+		{
+			echo -e "CheckTool 参数错误!!"
+			return 255
+		}
+	ToolPath=$(which $1)
 	[ -e "$ToolPath" ] ||
-	{
-		 echo -e "$1 不存在，请先安装此工具!!!";
-		 return 255;
-	};
-	return 0;
+		{
+			echo -e "$1 不存在，请先安装此工具!!!"
+			return 255
+		}
+	return 0
 }
 
 export HENVBOX_UNSUPPORTED=0
@@ -38,21 +37,20 @@ CheckTool mkdir
 CheckTool xargs
 [ $? -eq 0 ] || export HENVBOX_UNSUPPORTED=1
 CheckTool sudo
-[ $? -eq 0 ] || [ `id -u` -eq 0 ] ||  export HENVBOX_UNSUPPORTED=1
+[ $? -eq 0 ] || [ $(id -u) -eq 0 ] || export HENVBOX_UNSUPPORTED=1
 
 #某些系统中，若存在GNU版本工具则切换至GNU工具
-function CheckTool2
-{
-    [  -n "$1"  ] ||
-    {
-        return 255;
-    };
-    ToolPath=`which $1`;
-    [ -e "$ToolPath" ] ||
-    {
-         return 255;
-    };
-    return 0;
+function CheckTool2 {
+	[ -n "$1" ] ||
+		{
+			return 255
+		}
+	ToolPath=$(which $1)
+	[ -e "$ToolPath" ] ||
+		{
+			return 255
+		}
+	return 0
 }
 CheckTool2 guname
 [ $? -eq 0 ] && alias uname=guname
@@ -75,43 +73,41 @@ CheckTool2 gmkdir
 CheckTool2 gxargs
 [ $? -eq 0 ] && alias xargs=gxargs
 
-
-
-if [ ${HENVBOX_UNSUPPORTED} -ne 1 ]
-then
+if [ ${HENVBOX_UNSUPPORTED} -ne 1 ]; then
 	# shellcheck disable=SC2128  # ignore array expansion warning
-	if [ -n "${BASH_SOURCE-}" ]
-	then
-	self_path="${BASH_SOURCE}"
-	elif [ -n "${ZSH_VERSION-}" ]
-	then
-	self_path="${(%):-%x}"
+	if [ -n "${BASH_SOURCE-}" ]; then
+		self_path="${BASH_SOURCE}"
+	elif [ -n "${ZSH_VERSION-}" ]; then
+		self_path="${(%):-%x}"
 	else
 		return 1
 	fi
 
 	# shellcheck disable=SC2169,SC2169,SC2039  # unreachable with 'dash'
 	if [[ "$OSTYPE" == "darwin"* ]]; then
-	# convert possibly relative path to absolute
-	script_dir="$(realpath_int "${self_path}")"
-	# resolve any ../ references to make the path shorter
-	script_dir="$(cd "${script_dir}" || exit 1; pwd)"
+		# convert possibly relative path to absolute
+		script_dir="$(realpath_int "${self_path}")"
+		# resolve any ../ references to make the path shorter
+		script_dir="$(
+			cd "${script_dir}" || exit 1
+			pwd
+		)"
 	else
-	# convert to full path and get the directory name of that
-	script_name="$(readlink -f "${self_path}")"
-	script_dir="$(dirname "${script_name}")"
+		# convert to full path and get the directory name of that
+		script_name="$(readlink -f "${self_path}")"
+		script_dir="$(dirname "${script_name}")"
 	fi
-	
+
 	#导出根路径
-	export HENVBOX_ROOT_PATH="${script_dir}";
-	
+	export HENVBOX_ROOT_PATH="${script_dir}"
+
 	#导出类型
-	export HENVBOX_TYPE=`uname -s`
-	
+	export HENVBOX_TYPE=$(uname -s)
+
 	#导出uid与gid
-	export HENVBOX_UID=`id -u`
-	export HENVBOX_GID=`id -g`
-	
+	export HENVBOX_UID=$(id -u)
+	export HENVBOX_GID=$(id -g)
+
 	mkdir -p ${HENVBOX_ROOT_PATH}/local/bin
 	export HENVBOX_LOCAL_ROOT_PATH=${HENVBOX_ROOT_PATH}/local/
 	export HENVBOX_LOCAL_ROOT_PATH_UNIX=${HENVBOX_LOCAL_ROOT_PATH}
@@ -122,24 +118,20 @@ then
 	export PATH=${PATH}:${HENVBOX_LOCAL_BINDIR_PATH_UNIX}
 
 	#导入local目录下的其它软件(如ct-ng编译的交叉编译工具链)
-	for bin in `find ${HENVBOX_LOCAL_ROOT_PATH_UNIX} -mindepth 2 -maxdepth 2 -type d -name bin 2> /dev/null`
-	do
+	for bin in $(find ${HENVBOX_LOCAL_ROOT_PATH_UNIX} -mindepth 2 -maxdepth 2 -type d -name bin 2>/dev/null); do
 		export PATH=${PATH}:${bin}
 	done
 
 	#导入tools中的配置脚本
-	if [ -x "${HENVBOX_ROOT_PATH}/tools/${HENVBOX_TYPE}/config.sh" ]
-	then
+	if [ -x "${HENVBOX_ROOT_PATH}/tools/${HENVBOX_TYPE}/config.sh" ]; then
 		. "${HENVBOX_ROOT_PATH}/tools/${HENVBOX_TYPE}/config.sh"
 	fi
 
-	if [ "${CONFIG_HENVBOX_TOOLS_PATH_PRIORITY_HIGH}" = "y" ]
-	then
-        	export PATH=${HENVBOX_LOCAL_BINDIR_PATH_UNIX}:${PATH}
-		for bin in `find ${HENVBOX_LOCAL_ROOT_PATH_UNIX} -mindepth 2 -maxdepth 2 -type d -name bin 2> /dev/null`
-        	do
-                	export PATH=${bin}:$PATH
-        	done
+	if [ "${CONFIG_HENVBOX_TOOLS_PATH_PRIORITY_HIGH}" = "y" ]; then
+		export PATH=${HENVBOX_LOCAL_BINDIR_PATH_UNIX}:${PATH}
+		for bin in $(find ${HENVBOX_LOCAL_ROOT_PATH_UNIX} -mindepth 2 -maxdepth 2 -type d -name bin 2>/dev/null); do
+			export PATH=${bin}:$PATH
+		done
 	fi
 else
 	echo 无法完成HEnvBox配置!
--- upgrade.sh
+++ upgrade.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-function CheckTool
-{
-        [  -n "$1"  ] ||
-        {
-                echo -e  "CheckTool 参数错误!!";
-                return 255;
-        };
-        ToolPath=`which $1`;
-        [ -e "$ToolPath" ] ||
-        {
-                 echo -e "$1 不存在，请先安装此工具!!!";
-                 return 255;
-        };
-        return 0;
+function CheckTool {
+	[ -n "$1" ] ||
+		{
+			echo -e "CheckTool 参数错误!!"
+			return 255
+		}
+	ToolPath=$(which $1)
+	[ -e "$ToolPath" ] ||
+		{
+			echo -e "$1 不存在，请先安装此工具!!!"
+			return 255
+		}
+	return 0
 }
 
 export HENVBOX_UNSUPPORTED=0
@@ -38,51 +37,48 @@ CheckTool mkdir
 CheckTool git
 [ $? -eq 0 ] || export HENVBOX_UNSUPPORTED=1
 CheckTool sudo
-[ $? -eq 0 ] || [ `id -u` -eq 0 ] ||  export HENVBOX_UNSUPPORTED=1
+[ $? -eq 0 ] || [ $(id -u) -eq 0 ] || export HENVBOX_UNSUPPORTED=1
 
-if [ ${HENVBOX_UNSUPPORTED} -ne 1 ]
-then
-        # shellcheck disable=SC2128  # ignore array expansion warning
-        if [ -n "${BASH_SOURCE-}" ]
-        then
-        self_path="${BASH_SOURCE}"
-        elif [ -n "${ZSH_VERSION-}" ]
-        then
-        self_path="${(%):-%x}"
-        else
-                return 1
-        fi
+if [ ${HENVBOX_UNSUPPORTED} -ne 1 ]; then
+	# shellcheck disable=SC2128  # ignore array expansion warning
+	if [ -n "${BASH_SOURCE-}" ]; then
+		self_path="${BASH_SOURCE}"
+	elif [ -n "${ZSH_VERSION-}" ]; then
+		self_path="${(%):-%x}"
+	else
+		return 1
+	fi
 
-        # shellcheck disable=SC2169,SC2169,SC2039  # unreachable with 'dash'
-        if [[ "$OSTYPE" == "darwin"* ]]; then
-        # convert possibly relative path to absolute
-        script_dir="$(realpath_int "${self_path}")"
-        # resolve any ../ references to make the path shorter
-        script_dir="$(cd "${script_dir}" || exit 1; pwd)"
-        else
-        # convert to full path and get the directory name of that
-        script_name="$(readlink -f "${self_path}")"
-        script_dir="$(dirname "${script_name}")"
-        fi
+	# shellcheck disable=SC2169,SC2169,SC2039  # unreachable with 'dash'
+	if [[ "$OSTYPE" == "darwin"* ]]; then
+		# convert possibly relative path to absolute
+		script_dir="$(realpath_int "${self_path}")"
+		# resolve any ../ references to make the path shorter
+		script_dir="$(
+			cd "${script_dir}" || exit 1
+			pwd
+		)"
+	else
+		# convert to full path and get the directory name of that
+		script_name="$(readlink -f "${self_path}")"
+		script_dir="$(dirname "${script_name}")"
+	fi
 
-        #导出根路径
-        export HENVBOX_ROOT_PATH="${script_dir}";
+	#导出根路径
+	export HENVBOX_ROOT_PATH="${script_dir}"
 
 	#更新代码（若采用git管理源代码）
 	export HENVBOX_UPGRADE=1
-	if [ -d "${HENVBOX_ROOT_PATH}/.git" ]
-	then
+	if [ -d "${HENVBOX_ROOT_PATH}/.git" ]; then
 		pushd "${HENVBOX_ROOT_PATH}"
 		git pull
 		popd
 	fi
 
-        #使用安装脚本
-        if [ -x "${HENVBOX_ROOT_PATH}/install.sh" ]
-        then
-                . "${HENVBOX_ROOT_PATH}/install.sh"
-        fi
+	#使用安装脚本
+	if [ -x "${HENVBOX_ROOT_PATH}/install.sh" ]; then
+		. "${HENVBOX_ROOT_PATH}/install.sh"
+	fi
 else
-        echo 无法完成HEnvBox配置!
+	echo 无法完成HEnvBox配置!
 fi
-

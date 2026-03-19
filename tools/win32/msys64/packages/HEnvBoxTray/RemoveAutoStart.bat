62a5e6b8da53acabf8296debef50f86343bdc004
HEYAHONG <2229388563@qq.com>
2026-03-19 14:38:22 +0800

    [HEnvBoxTray] 完善卸载脚本

---
+++ tools/win32/msys64/packages/HEnvBoxTray/RemoveAutoStart.bat
@@ -0,0 +1,5 @@
+@echo off
+
+if exist "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\HEnvBoxTray.bat" del /s /q "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\HEnvBoxTray.bat"
+
+exit 0
